728c67d9064f23ba699e6eaf686942e764d2d276
Seigo.k <216270240+seigo-gace@users.noreply.github.com>
2026-09-16 19:29:40 +0900

    Allow 2FA management for OAuth and passwordless accounts

--- functions/_auth.ts
+++ functions/_auth.ts
@@ -124,6 +124,7 @@ export function createAuth(env: AuthEnv) {
     plugins: [
       twoFactor({
         issuer: 'Astera',
+        allowPasswordless: true,
         otpOptions: {
           period: 300,
           async sendOTP({ user, otp }) {
